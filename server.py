236dc07e90f71590a1152a541829a2fc48c263dc
BITMAIN27 <166276283+BITMAIN27@users.noreply.github.com>
2024-11-09 22:15:07 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,39 @@
 from flask import Flask, request, jsonify
+import random
 
 
 app = Flask(__name__)
 
-@app.route('/get_question', methods=['GET'])
+def determine_question(index):
+    current_index = index
+    question_list = []
+    with open("Questions.txt",'r') as file:
+        for question in file:
+            question_list.append(question.strip())
+
+    return question_list[index]
+
+"""@app.route('/get_question', methods=['GET'])
 def send_question():
+    question_index = 0
+    
     # The key is now 'message' to match what React expects
     data = {
         "message": ""
     }
     #make logic here to choose question
-    data['message'] = "Custom message from sever"
+    question = determine_question(question_index)
+    data['message'] = question
     
-    return jsonify(data)
+    return jsonify(data)"""
+
+@app.route('/get_question',methods=['POST'])
+def get_question():
+    data = request.get_json()
+    new_index = data.get('index')
+    question = determine_question(new_index)
+    
+    return jsonify({'message': 'sent question', 'data': question})
 
 @app.route('/proccess_audio',methods=['POST'])
 def process_data():
