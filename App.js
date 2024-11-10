ab5e6c7db66ee8e02eb675baab9ab63af467aec4
BITMAIN27 <166276283+BITMAIN27@users.noreply.github.com>
2024-11-09 19:58:43 -0500

    Update App.js
    Added a way to send data back to Flask sever

--- App.js
+++ App.js
@@ -1,19 +1,41 @@
-
-
-import react, {useState,useEffect} from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState([{}])
+  const [responseMessage, setResponseMessage] = useState('');
 
-  useEffect(()=> {
-    fetch("/members").then(res => res.json()).then(data=> {setData(data); console.log(data);})}
-    ,[])
+  useEffect(() => {
+    // Hardcoded data
+
+    const transcript = {
+        transcript: "My string transcript"
+    };
+    const sendTranscriptToFlask = async () => {
+      try{
+        const response = await axios.post('/proccess_audio',transcript)
+        setResponseMessage(response.data.message);  // Display success message
+        console.log(response.data);  // Log the entire response
+
+      } catch (error)
+      {
+        setResponseMessage('Error submitting data');
+        console.error('Error:', error);
+      }
+    };
+
+    sendTranscriptToFlask();
+
+  }, []);
 
   return (
-    <div>
-      Flask test server
+    <div className="App">
+      <h1>Sending Hardcoded Data to Flask</h1>
+      <div>
+        <h3>{responseMessage}</h3>  {/* Display response from Flask */}
+      </div>
     </div>
   );
 }
 
 export default App;
+
