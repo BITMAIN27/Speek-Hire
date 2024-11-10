362a42f861527677905c7eba8574b0908d41946b
BITMAIN27 <166276283+BITMAIN27@users.noreply.github.com>
2024-11-09 19:59:56 -0500

    Update server.py
    Server is now set to receive data from react web app

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
 
 app = Flask(__name__)
 
-@app.route("/members")
-def members():
-    return{"members": ["Member1","Member2","Member3"]}
+
+@app.route('/proccess_audio',methods=['POST'])
+def process_data():
+    data = request.get_json()
+    transcript = data.get('transcript')
+    
+    
+    #process interview here
+    
+    
+    print(transcript)
+    return jsonify({'message': 'Hello from server', 'data': data})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
