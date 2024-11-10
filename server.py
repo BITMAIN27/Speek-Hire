a9f822c7434ece335be7bceef80c33ff511a367f
BITMAIN27 <166276283+BITMAIN27@users.noreply.github.com>
2024-11-09 20:28:41 -0500

    Update server.py
    Updated server to send and get messages from react WP

--- server.py
+++ server.py
@@ -3,6 +3,16 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/get_question', methods=['GET'])
+def send_question():
+    # The key is now 'message' to match what React expects
+    data = {
+        "message": ""
+    }
+    #make logic here to choose question
+    data['message'] = "Custom message from sever"
+    
+    return jsonify(data)
 
 @app.route('/proccess_audio',methods=['POST'])
 def process_data():
